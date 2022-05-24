e001a762953bb4dec4d21baa4a37518a1392bb77
Lucas Ferreira <lucasfpnt@gmail.com>
2022-05-24 15:48:37 -0300

    requisto 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,29 @@
 export async function getCategories() {
-  // Implemente aqui
-}
+  const categorias = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories',
+  )
+    .then((response) => response.json())
+    .then((categoria) => categoria);
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  return categorias;
+}
+// Para buscar por itens por termo:
+// https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
+
+// Para buscar itens por categoria:
+// https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID
+
+// Para buscar itens de uma categoria por termo
+// https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
+
+// Para buscar detalhes de um item especifico:
+// https://api.mercadolibre.com/items/$PRODUCT_ID
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const products = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  )
+    .then((resp) => resp.json())
+    .then((json) => json.results);
+  return products;
 }
